083602f38f644e86c6efa101337e74c9f7f6719b
Husain-Jinia <75819874+Husain-Jinia@users.noreply.github.com>
2021-02-20 11:57:06 +0530

    initial commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,13 @@
+<?php include('partials/menu.php');?>
+
+        <!--  Main Content section starts -->
+        <div class="main-content">
+            <div class="wrapper">
+               <h1>Manage Admin</h1>
+
+               
+            </div>
+        </div>
+        <!-- Main Content section ends -->
+
+<?php include('partials/footer.php');?>
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+ <!-- Footer section starts -->
+ <div class="footer">
+            <div class="wrapper">
+                <p class = "text-center">2020 All rights reserved<p>
+            </div>
+        </div>
+        <!-- Footer section ends -->
+    </body>
+</html>
